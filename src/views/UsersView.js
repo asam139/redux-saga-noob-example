e1dbfb70cc554496eca3a05cc5b9e6bf9239032b
jj <jperez@celtiberian.es>
2021-09-14 20:54:44 +0200

    Add some styles to table

--- src/views/UsersView.js
+++ src/views/UsersView.js
@@ -31,28 +31,57 @@ const UsersView = () => {
   const usersCountFooterText = useMemo(() => `${users.length} users found`, [users]);
 
   return (
-    <>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}
+    >
       {loading ? (
         <LoadingSpinner />
       ) : (
-        <List
-          header={
-            <Search placeholder="Type to search by email" onChange={onChange} onSearch={onSearch} enterButton />
-          }
-          footer={<div>{usersCountFooterText}</div>}
-          bordered
-          dataSource={users}
-          renderItem={({ avatar, email }) => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={<Avatar src={avatar} />}
-                title={email}
-              />
-            </List.Item>
-          )}
-        />
+        <div style={{ width: '80vh', height: '60vh' }}>
+          <List
+            header={
+              <Search placeholder="Type to search by email" onChange={onChange} onSearch={onSearch} enterButton />
+            }
+            footer={(
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'flex-end',
+                }}
+              >
+                {usersCountFooterText}
+              </div>
+            )}
+            grid={{ column: 2 }}
+            bordered
+            dataSource={users}
+            renderItem={({ avatar, email }) => (
+              <List.Item style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: '0px',
+                marginBotton: '0px',
+                paddingTop: '8px',
+                paddingBottom: '8px',
+              }}
+              >
+                <List.Item.Meta
+                  style={{ alignItems: 'center' }}
+                  avatar={<Avatar src={avatar} />}
+                  title={email}
+                />
+              </List.Item>
+            )}
+          />
+        </div>
       )}
-    </>
+    </div>
   );
 };
 
