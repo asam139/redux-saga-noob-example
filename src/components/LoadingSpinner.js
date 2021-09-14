27e15090f9fbbb54dbeca469f7f817810cb68233
jj <jperez@celtiberian.es>
2021-09-14 20:24:48 +0200

    LoadingSpinner component

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Spin } from 'antd';
+
+const LoadingSpinner = () => (
+  <div style={{
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }}
+  >
+    <Spin size="large" />
+  </div>
+);
+
+export default LoadingSpinner;
